19d9c985852e6831ec9068920569939069f332f4
Qamrul-Hassan <sajal700@gmail.com>
2025-02-04 15:43:59 +0600

    massage

--- build.js
+++ build.js
@@ -1,6 +1,6 @@
 const { exec } = require('child_process');
 
-exec('npx tailwindcss -i ./src/input.css -o ./public/output.css --minify', (err, stdout, stderr) => {
+exec('npx tailwindcss -i ./pubic/input.css -o ./public/output.css --minify', (err, stdout, stderr) => {
   if (err) {
     console.error(`Error executing build: ${stderr}`);
     process.exit(1);
